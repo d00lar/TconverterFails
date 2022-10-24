92a3ecce5b24b37641b9a400f429ec87a7078619
emirsdor <dorian.emirski@orange.com>
2022-10-24 08:36:00 +0200

    Add project files.

---
+++ Shared/IntStrDict.cs
@@ -0,0 +1,27 @@
+﻿namespace MyApplication.Shared
+{
+    public class IntStrDict
+    {
+        public List<KeyValuePair<int, string>> Values { get; set; } = new();
+
+
+
+        public string GetVal(int? Key)
+        {
+            if (Values.Count == 0) return "Loading..";
+
+            try
+            {
+                return Values.Where(x => x.Key == Key).Single().Value;
+            }
+            catch (Exception)
+            {
+
+                return "NotMappedDictValue";
+            }
+
+
+        }
+
+    }
+}
